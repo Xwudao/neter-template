af6452e763f72560d7ac8138e6e559b811d6979d
Tim <apkapb@gmail.com>
2026-09-17 12:34:28 +0800

    chore(enum): remove enumer generate directives

--- pkg/enum/constants.go
+++ pkg/enum/constants.go
@@ -1,10 +1,5 @@
 package enum
 
-// go install github.com/alvaroloes/enumer@latest
-
-//go:generate enumer -type=HttpCode -json
-//go:generate enumer -type=Reason -json
-
 type HttpCode int
 
 const (
